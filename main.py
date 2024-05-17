44138b5778c8f04508e489cbd034a2192e71871a
Foo Ming Kuang <150900178+mkuangdotcom@users.noreply.github.com>
2024-05-17 23:59:53 +0800

    Image Processing File Update

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ cap = cv2.VideoCapture(0)  # use the first camera device it finds.
 
 while True:
     ret, frame = cap.read()  # Capture frame-by-frame, ret indicates if the frame was captured successfully (boolean), frame NumPy array containing the pixel data for the frame.
+    if ret is False:
+        print("Error: Can't receive frame")
+        break
+    
+    cv2.imshow('Frame', frame)  # Display the frame, window automatically fits to the image size.
+    
+    key = cv2.waitKey(1) # Wait for a key press, 1ms delay.
+    if key == 27:
+        break
+
+# Release the camera and destroy all windows after the loop has finished
+cap.release()
+cv2.destroyAllWindows()
+    
